be354cdf241c576b068c5cec92dc9ffc67e44001
lilblueyes <101664783+lilblueyes@users.noreply.github.com>
2026-03-04 15:07:00 +0100

    Restore launch namespace and backend options

--- launch/audio_server.launch.py
+++ launch/audio_server.launch.py
@@ -8,7 +8,9 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+    namespace = LaunchConfiguration("namespace")
     mode = LaunchConfiguration("mode")
+    backend = LaunchConfiguration("backend")
     config_file = LaunchConfiguration("config_file")
 
     default_cfg = PathJoinSubstitution(
@@ -18,16 +20,19 @@ def generate_launch_description():
     return LaunchDescription(
         [
             DeclareLaunchArgument("namespace", default_value=""),
-            DeclareLaunchArgument("mode", default_value="normal"),
+            DeclareLaunchArgument("mode", default_value="normal", description="normal|debug|silent|mute"),
+            DeclareLaunchArgument("backend", default_value="auto", description="auto|player|null"),
             DeclareLaunchArgument("config_file", default_value=default_cfg),
             Node(
                 package="audio_manager_ros2",
                 executable="audio_server_node",
                 name="audio_server",
+                namespace=namespace,
                 output="screen",
                 parameters=[
                     {
                         "mode": ParameterValue(mode, value_type=str),
+                        "backend": ParameterValue(backend, value_type=str),
                         "config_file": ParameterValue(config_file, value_type=str),
                     }
                 ],
